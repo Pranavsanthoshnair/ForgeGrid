708eccb3fb65b402e5fc179d8afafc3ae38eccfe
Keerthana-Sjcet <keerthanagpillai2028@cs.sjcetpalai.ac.in>
2025-10-10 20:35:33 +0530

    Hide and Seek

--- run.bat
+++ run.bat
@@ -1,4 +1,23 @@
 @echo off
+setlocal
+
+REM Change to the directory where this script is located
+cd /d "%~dp0"
+
+REM Ensure build output exists
+if not exist "bin" (
+  echo bin directory not found. Please run build.bat first.
+  exit /b 1
+)
+
+REM Prefer our freshly built classes and local libs
+set "CP=bin;lib\*"
+
+echo Running ForgeGrid...
+java -cp "%CP%" com.forgegrid.app.Main
+
+endlocal
+@echo off
 setlocal enabledelayedexpansion
 
 REM Change to the directory where the batch file is located
